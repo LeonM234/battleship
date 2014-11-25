6adaf57ca518c5e3974bcdfe7480b6bb05018426
Leon Peck <leonpeck234@gmail.com>
2014-11-25 10:27:50 -0600

    Added JS board creator

--- main.js
+++ main.js
@@ -1,5 +1,56 @@
+var ship = [
+    {shipName: 'aircraft-carrier', numberofSquares: 5, hitCounter: 0, alignment: 1, matrixType: 5, destroyed: 0},
+    {shipName: 'battleship', numberofSquares: 4, hitCounter: 0, alignment: 1, matrixType: 4, destroyed: 0 },
+    {shipName: 'submarine', numberofSquares: 3, hitCounter: 0, alignment: 1, matrixType: 3, destroyed: 0 },
+    {shipName: 'destroyer', numberofSquares: 3, hitCounter: 0, alignment: 1, matrixType: 2, destroyed: 0 },
+    {shipName: 'patrol-boat', numberofSquares: 2, hitCounter: 0, alignment: 1, matrixType: 1, destroyed: 0 },]
+
+var matrix = [[0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0]];
+
+matrix.forEach(creatingRows);
+  function creatingRows(rowValue){
+    var table = document.querySelector('#table1');
+    var $tr = document.createElement('tr');
+    table.appendChild($tr);
+    rowValue.forEach(function(cellValue) {
+    var $td = document.createElement('td');
+    $td.textContent = cellValue;
+    if (cellValue === 0){
+      $td.classList.add('miss');
+    } else {
+      $td.classList.add('hit');
+    }
+    $tr.appendChild($td);
+  });
+}
+
+
 // Dragging / Dropping Ships
 //  TO-DO: Snap items into place
+
+// ----- TABLE TD'S DROPPABLE -----
+    var $space = $("#table1 td");
+    
+    $space.droppable({
+      drop: function(event, ui){
+        console.log("dropped onto space");
+        var $space = $(this);
+        }
+      });
+
+// ----- TABLE DROPPABLE -----
+$(function(){
+  $('#table1').droppable();
+});
 
 // ----- SHIPS DRAGGABLE -----
 $(function() {
@@ -13,11 +64,6 @@ $(function() {
       return !event;
     }
   });
-});
-
-// ----- TABLE DROPPABLE -----
-$(function(){
-  $('#table1').droppable();
 });
 
 // ----- SELECT SHIP W/ CLICK -----
