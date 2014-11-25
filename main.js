6c4c90503b0db7c2bd29cfbce7e55b602e13291e
Leon Peck <leonpeck234@gmail.com>
2014-11-25 09:50:18 -0600

    Committing current board before potential refactor to JS generated board

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Dragging / Dropping Ships
-//  TO-DO:  Add the rest of the ships
-//          Snap items into place
+//  TO-DO: Snap items into place
 
+// ----- SHIPS DRAGGABLE -----
 $(function() {
   $('#aircraft-carrier, #battleship, #submarine, #destroyer, #patrol').draggable({
     // If dropped outside of table, revert to fleet-box
@@ -15,17 +15,42 @@ $(function() {
   });
 });
 
+// ----- TABLE DROPPABLE -----
 $(function(){
-  $('#table1').droppable();
   $('#table1').droppable();
 });
 
-// On clicking icon 
+// ----- SELECT SHIP W/ CLICK -----
+// Click a ship to select it
+$('#aircraft-carrier, #battleship, #submarine, #destroyer, #patrol').on('click', function(){
+  // Highlight via adding class "selected"
+    $(this).addClass("selected");
+  // Click another ship and un-hilight previous one, and highlight new one
+    
+});
+//    Pass selected ship into a variable to hold
+//    Use selected to determine which ship to flip if icon clicked
+//
+
+// ----- FLIP SHIPS HORIZ/VERT -----
+// On clicking icon flip selected ship
 $('#arrow').on('click', function(){
   // selected ship flips between horizontal and vertical
-  var aircraft = $('#aircraft-carrier');
+  
+  // for each ship, find the one with class selected
+  var ships = [$('#aircraft-carrier'), $('#battleship'), $('#submarine'), $('#destroyer'), $('#patrol')];
 
-  aircraft.rotate({animateTo:90});  
+  /*ships.forEach(){
+    console.log($(this) + " is a ship!");
+  }*/
+      // set the width of the selected ship equal to height, and height equal to width
+  
+  /*var aircraft = $('#aircraft-carrier');
+
+  aircraft.css({
+    "width": "140px",
+    "height": "28px"
+  });*/
 });
 
 // Turn off ability to move once snapped in place
