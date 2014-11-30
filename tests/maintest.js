3dfb429700a9c15a06d73254fc3af06af70a4231
SondaSengupta <sondasengupta@gmail.com>
2014-11-29 18:01:15 -0600

    Tested hit status of ships using index values

--- tests/maintest.js
+++ tests/maintest.js
@@ -1,24 +1,43 @@
 var ship = [
-    {shipName: 'aircraft-carrier', numberofSquares: 5, hitCounter: 0, alignment: 1, matrixType: 5, destroyed: 0},
-    {shipName: 'battleship', numberofSquares: 4, hitCounter: 0, alignment: 1, matrixType: 4, destroyed: 0 },
-    {shipName: 'submarine', numberofSquares: 3, hitCounter: 0, alignment: 1, matrixType: 3, destroyed: 0 },
-    {shipName: 'destroyer', numberofSquares: 3, hitCounter: 0, alignment: 1, matrixType: 2, destroyed: 0 },
-    {shipName: 'patrol-boat', numberofSquares: 2, hitCounter: 0, alignment: 1, matrixType: 1, destroyed: 0 },]
+    {shipName: 'aircraft-carrier', numberofSquares: 5, hitCounter: 0, alignment: 1, indexType: 0, destroyed: 0},
+    {shipName: 'battleship', numberofSquares: 4, hitCounter: 0, alignment: 1, indexType: 1, destroyed: 0 },
+    {shipName: 'submarine', numberofSquares: 3, hitCounter: 0, alignment: 1, indexType: 2, destroyed: 0 },
+    {shipName: 'destroyer', numberofSquares: 3, hitCounter: 0, alignment: 1, indexType: 3, destroyed: 0 },
+    {shipName: 'patrol-boat', numberofSquares: 2, hitCounter: 0, alignment: 1, indexType: 4, destroyed: 0 },]
 
-var testMatrix = [[5,5,5,5,5,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0,0,0],
-              [3,3,3,0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0,0,0],
-              [2,2,2,0,0,0,0,0,0,4],
-              [0,0,0,0,0,0,0,0,0,4],
-              [0,0,0,0,0,0,0,0,0,4],
-              [0,0,0,0,0,0,0,0,0,4],
-              [1,1,0,0,0,0,0,0,0,0]];
+var testMatrix = [[0,0,0,0,0,9,9,9,9,9],
+              [9,9,9,9,9,9,9,9,9,9],
+              [9,9,9,9,9,9,9,9,9,9],
+              [2,2,2,9,9,9,9,9,9,9],
+              [9,9,9,9,9,9,9,9,9,9],
+              [3,3,3,9,9,9,9,9,9,1],
+              [9,9,9,9,9,9,9,9,9,1],
+              [9,9,9,9,9,9,9,9,9,1],
+              [9,9,9,9,9,9,9,9,9,1],
+              [4,4,9,9,9,9,9,9,9,9]];
 
-function hitOrMiss(enemyTargetX, enemyTargetY){
-  var enemyTargetValue = testMatrix[enemyTargetX][enemyTargetY];
-  if (enemyTargetValue === 0){
+function whatEnemyHit(enemyTargetValue){
+  hitOrMiss(enemyTargetValue);
+  if (enemyTargetValue === 9){
+        return ("You missed!");
+      }
+  else {
+    name = ship[enemyTargetValue].shipName;
+    return name;
+  }
+}
+
+function hitIncrease(enemyTargetValue){
+  var hitCounter = ship[enemyTargetValue].hitCounter += 1;
+  if ( hitCounter >= ship[enemyTargetValue].numberofSquares){
+    ship[enemyTargetValue].destroyed = 1;
+    return ("You have sunk the " + ship[enemyTargetValue].shipName + "!");
+  }
+  return hitCounter;
+}
+
+function hitOrMiss(enemyTargetValue){
+  if (enemyTargetValue === 9){
     return ('You missed!');
   } else {
     return ('Hit!');
@@ -26,7 +45,8 @@ function hitOrMiss(enemyTargetX, enemyTargetY){
 };
 
 function whatinLocation(enemyTargetX, enemyTargetY){
-  var enemyTargetValue = testMatrix[enemyTargetX][enemyTargetY];
+  var enemyTargetValue = testMatrix[enemyTargetX][enemyTargetY]
+  testMatrix[enemyTargetX][enemyTargetY] = 9;
   return enemyTargetValue;
 };
 
